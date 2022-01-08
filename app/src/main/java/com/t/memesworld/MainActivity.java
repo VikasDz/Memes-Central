cd213095e4d4e8a9b1bccacd2470d15f7919c7c4
Satya <vikashraj979898@gmail.com>
2022-01-08 21:23:59 +0530

    Auth Done

--- app/src/main/java/com/t/memesworld/MainActivity.java
+++ app/src/main/java/com/t/memesworld/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,12 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser mCurrentUser;
-    private Button mLogoutBtn;
+    private FloatingActionButton mLogoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
 
